3ef2c14098b3afd0a4a9d44bde9a1447379c67ac
Kaung Myat Htay Win <kaungmyathtaywin@john.lan>
2024-07-05 23:22:58 -0700

    Add CORS to send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 const nodemailer = require('nodemailer')
 
-
 export default (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
+    // Handle preflight request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
     const { name, email, subject, message } = req.body
 
     const transporter = nodemailer.createTransport({
